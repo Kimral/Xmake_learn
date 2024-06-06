2e620b437669c31c5d39722ebdb5e1d47b8c937a
Kimral <semaytes2012@mail.ru>
2024-06-06 21:39:14 +0500

    imgui linked

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,8 @@
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 add_rules("mode.debug", "mode.release")
 
 add_requires("catch2 3.x")
+add_requires("imgui", {configs = {glfw_opengl3 = true}})
 
 set_plat(os.host())
 set_arch(os.arch())
@@ -8,6 +10,8 @@ set_arch(os.arch())
 target("App")
     set_kind("binary")
     set_version("0.0.1")
+
+    add_packages("imgui")
     
     add_files("src/*.cpp")
 target_end()
@@ -16,6 +20,8 @@ target("Tests")
     set_kind("binary")
 
     add_packages("catch2")
+
+    add_packages("imgui")
 
     remove_files("src/main.cpp")
 
