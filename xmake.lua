0f629080efd52ade3e278f748e1a42adc4e8520a
Kimral <semaytes2012@mail.ru>
2024-06-06 21:56:22 +0500

    xmake.lua additional inner target

--- xmake.lua
+++ xmake.lua
@@ -7,24 +7,31 @@ add_requires("imgui", {configs = {glfw_opengl3 = true}})
 set_plat(os.host())
 set_arch(os.arch())
 
+target("__Inner_targer")
+    set_kind("static")
+
+    set_default(false)
+
+    add_packages("imgui")
+    add_files("src/**.cpp")
+target_end()
+
+
 target("App")
     set_kind("binary")
     set_version("0.0.1")
 
-    add_packages("imgui")
-    
-    add_files("src/*.cpp")
+    add_deps("__Inner_targer")
+
 target_end()
+
 
 target("Tests")
     set_kind("binary")
 
     add_packages("catch2")
 
-    add_packages("imgui")
+    add_deps("__Inner_targer")
 
-    remove_files("src/main.cpp")
-
-    add_files("src/*.cpp")
-    add_files("tests/*.cpp")
+    add_files("tests/**.cpp")
 target_end()
