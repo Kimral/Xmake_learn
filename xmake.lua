32a2c8cabb1344021ab9115e5a34a766fa2d8ae9
Kimral <semaytes2012@mail.ru>
2024-06-11 19:33:57 +0500

    deprecated config flags replaced

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 add_rules("mode.debug", "mode.release")
 
 add_requires("catch2 3.x")
-add_requires("imgui", {configs = {glfw_opengl3 = true}})
+add_requires("imgui", {configs = {glfw = true, opengl3 = true}})
 
 set_plat(os.host())
 set_arch(os.arch())
