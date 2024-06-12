36e8fed216b277a211ec5855003d4535fa3cd0ce
Kimral <semaytes2012@mail.ru>
2024-06-12 22:20:32 +0500

    Structure rework

--- xmake.lua
+++ xmake.lua
@@ -1,36 +1,12 @@
-add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
 add_rules("mode.debug", "mode.release")
 
-add_requires("catch2 3.x")
-add_requires("imgui", {configs = {glfw = true, opengl3 = true}})
+set_languages("c++20")
 
-set_plat(os.host())
-set_arch(os.arch())
-
-target("__Inner_targer")
-    set_kind("static")
-    set_default(false)
-    set_languages("cxx23")
-    --#####
-    add_packages("imgui", {public = true})
-    add_includedirs("src/common", {public = true})
-    add_headerfiles("src/common/**.h")
-target_end()
-
+includes("src/*") -- Пoдключить все targets
 
 target("App")
     set_kind("binary")
-    set_version("0.0.1")
-    --#####
-    add_deps("__Inner_targer")
-    add_files("src/main.cpp")
-target_end()
-
-
-target("Tests")
-    set_kind("binary")
-    --#####
-    add_packages("catch2")
-    add_deps("__Inner_targer")
-    add_files("tests/**.cpp")
+    set_languages()
+    add_packages(common)
+    add_files("src/**.cpp")
 target_end()
