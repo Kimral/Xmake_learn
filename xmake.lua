f342bcf27452ec8badfaf0e11cc9f69473bfd7a6
Kimral <semaytes2012@mail.ru>
2024-06-13 23:27:18 +0500

    imgui null example from source

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,18 @@
+add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
+target("lib_imgui")
+    set_kind("static")
+    add_includedirs("src/imgui", {public = true})
+    add_files("src/imgui/*.cpp")
+target_end()
+
 target("App")
     set_kind("binary")
     set_languages()
-    add_packages(lib_catch2)
+    add_deps("lib_imgui")
     add_files("src/**.cpp")
 target_end()
