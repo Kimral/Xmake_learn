5cf2cf59491657a2d83c0040207190cf2d3b4b22
Kimral <semaytes2012@mail.ru>
2024-06-15 16:10:46 +0500

    Target "App" moved to common target's folder

--- xmake.lua
+++ xmake.lua
@@ -4,11 +4,4 @@ add_rules("mode.debug", "mode.release")
 
 set_languages("c++20")
 
-includes("src/*") -- Пoдключить все targets
-
-target("App")
-    set_kind("binary")
-    set_languages()
-    add_deps("lib_imgui")
-    add_files("src/**.cpp")
-target_end()
+includes("src/*")
