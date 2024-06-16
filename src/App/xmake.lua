24f8c0f9a15c459c42eddf21b4e80e7fe472c41a
Kimral <semaytes2012@mail.ru>
2024-06-16 21:37:51 +0500

    catch2 removed as unused

--- src/App/xmake.lua
+++ src/App/xmake.lua
@@ -1,6 +1,5 @@
 target("App")
     set_kind("binary")
-    set_languages()
     add_deps("lib_SDL2", "lib_imgui")
     add_files("src/*.cpp")
 target_end()
--- src/lib_catch2/xmake.lua
+++
@@ -1,6 +0,0 @@
-add_requires("catch2")
-
-target("lib_catch2")
-    set_kind("static")
-    add_packages("catch2", {public = true})
-target_end()
