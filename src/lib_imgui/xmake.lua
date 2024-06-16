0874398bba1b77887375cf24d4b96d54e535b64d
Kimral <semaytes2012@mail.ru>
2024-06-16 21:54:28 +0500

    code in mian changed

--- src/lib_imgui/xmake.lua
+++ src/lib_imgui/xmake.lua
@@ -1,5 +1,9 @@
 target("lib_imgui")
     set_kind("static")
-    add_includedirs("src/", {public = true})
+    add_includedirs("src/", "src/backends/", {public = true})
+
     add_files("src/*.cpp")
+    add_files("src/backends/imgui_impl_sdl2.cpp")
+    add_files("src/backends/imgui_impl_vulkan.cpp");
+
 target_end()
