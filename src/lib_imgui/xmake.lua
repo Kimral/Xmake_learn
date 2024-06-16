386d7e70fc951deec3c521ea4cf4f2c902c59041
Kimral <semaytes2012@mail.ru>
2024-06-16 23:41:23 +0500

    imgui as git submodule

--- src/lib_imgui/xmake.lua
+++ src/lib_imgui/xmake.lua
@@ -3,10 +3,10 @@ target("lib_imgui")
 
     add_deps("lib_vulkan", "lib_SDL2", {public = true})
 
-    add_includedirs("src/", "src/backends/", {public = true})
+    add_includedirs("imgui/", "imgui/backends/", {public = true})
 
-    add_files("src/*.cpp")
-    add_files("src/backends/imgui_impl_sdl2.cpp")
-    add_files("src/backends/imgui_impl_vulkan.cpp");
+    add_files("imgui/*.cpp")
+    add_files("imgui/backends/imgui_impl_sdl2.cpp")
+    add_files("imgui/backends/imgui_impl_vulkan.cpp");
 
 target_end()
