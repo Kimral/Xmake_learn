a01094547202b2f1301451edcab58e76cb2caa02
Kimral <semaytes2012@mail.ru>
2024-06-25 19:16:16 +0500

    Смена backend в runtime

--- src/App/src/Application/Application.cpp
+++ src/App/src/Application/Application.cpp
@@ -28,8 +28,6 @@ void Application::SetGuiBackend() {
     }
     m_CurrentHandler = m_HandlerRequired;
     m_CurrentRender = m_RenderRequired;
-    m_HandlerRequired = InputHandlers::NONE;
-    m_RenderRequired = Renders::NONE;
 }
 
 void Application::Init() {
@@ -43,45 +41,26 @@ void Application::Run() {
 }
 
 void Application::RunInner() {
-    static bool show_demo_window = true;
-    static bool show_another_window = false;
+    ImGui::SetNextWindowSize(ImVec2(600, 400));
+    ImGui::Begin("My window");
 
-    // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window)
-        ImGui::ShowDemoWindow(&show_demo_window);
+    static const char* weaponNamesForCombobox[] = {
+        "NONE",
+        "OpenGL3",
+        "Vulkan"
+    };
 
-    // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-    {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-        ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-        ImGui::Checkbox("Another Window", &show_another_window);
-
-        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);              // Edit 1 float using a slider from 0.0f to 1.0f
-        ImGui::ColorEdit3("clear color", (float*)&m_MyImgui->GetClearColor()); // Edit 3 floats representing a color
-
-        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / m_MyImgui->GetIO().Framerate, m_MyImgui->GetIO().Framerate);
-        ImGui::End();
+    // Convert the current enum value to an integer for ListBox
+    int currentIndex = static_cast<int>(m_RenderRequired);
+    if(ImGui::ListBox("##paintkit1", &currentIndex, weaponNamesForCombobox, IM_ARRAYSIZE(weaponNamesForCombobox), 8)) {
+        m_RenderRequired = static_cast<Renders>(currentIndex);
     }
 
-    // 3. Show another simple window.
-    if (show_another_window)
-    {
-        ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-        ImGui::Text("Hello from another window!");
-        if (ImGui::Button("Close Me"))
-            show_another_window = false;
-        ImGui::End();
+    if (ImGui::Button("Restart")) {
+        m_StartRequired = true;
     }
+
+    ImGui::End();
 }
 
 bool Application::IsStartRequired() {
@@ -93,6 +72,7 @@ void Application::SetStartRequired(bool value) {
 }
 
 void Application::SetReazation_SDL2(Renders render) {
+    m_MyImgui.reset();
     switch (render) {
         case Renders::OpenGL3: {
             m_MyImgui = std::make_unique<SDL2_Imgui<Opengl3_Render>>();
